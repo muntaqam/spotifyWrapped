f72745a0a7e1d2d12b33ad745571228db7c07ff2
muntaqam <maahi305@yahoo.com>
2023-08-07 22:34:28 -0400

    added front end react skeleton

---
+++ spotify-top100/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/Login'; // Adjust the path as needed
+
+function App() {
+  return (
+    <div>
+      <Login />
+      {/* Other components and content can go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-top100/src/components/Login.js
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+      <div className =" bg-">
+          <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+              <form class="space-y-6" action="#">
+                  <h3>Check my top Songs on Spotify </h3>
+              </form>
+          </div>
+      </div>
+    
+  );
+}
+
+export default Login;
