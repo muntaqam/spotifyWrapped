8fa23a7f0fb42f9e1a0f5f9f202fce8a397c5b68
muntaqam <maahi305@yahoo.com>
2023-08-09 18:23:23 -0400

    displayed songs and added simple styling continue to style..

--- spotify-top100/src/components/TopSongs.js
+++ spotify-top100/src/components/TopSongs.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
- 
+
 function TopSongs() {
   const [topSongs, setTopSongs] = useState([]);
 
@@ -17,22 +17,35 @@ function TopSongs() {
   console.log('topSongs:', topSongs);
 
   return (
-    <div>
+  <div className=''>
+    <div className="flex justify-center items-center">
       <h1>Top 100 Songs</h1>
+    </div>
+
+    <div className="bg-zinc-600 flex flex-col w-full border-opacity-50">
       {topSongs.length > 0 ? (
-        <ul>
-          {topSongs.map(song => (
-            <li key={song.id}>{song.name} - {song.artists.map(artist => artist.name).join(', ')}</li>
+        <ol>
+          {topSongs.map((song, index) => (
+            <li key={song.id} className="flex items-center text-white">
+              <span className="mr-2 text-gray-400">{index + 1}.</span>
+              <div>
+                {song.name} - {song.artists.map(artist => artist.name).join(', ')}
+              </div>
+            </li>
           ))}
-        </ul>
+        </ol>
       ) : (
         <p>Loading...</p>
       )}
     </div>
-  );
+  </div>
+);
+
 }
 
 export default TopSongs;
+
+
 
 // import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
